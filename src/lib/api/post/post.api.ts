c4a11c880cbf0f4c85504a9c65fda84ca84027fc
Eman Saeed <emansaeedhamed22220@gmail.com>
2026-04-12 01:08:49 +0200

    4

--- src/lib/api/post/post.api.ts
+++
@@ -1,7 +0,0 @@
-import { IPostResponse } from "@/lib/types/post";
-
-export async function getPost(){
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
-    const payload : IPostResponse = await response.json();
-    return payload
-}
